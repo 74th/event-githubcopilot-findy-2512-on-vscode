475c65b886db183ce092486c49a9018a169af75e
Atsushi Morimoto <74th.tech@gmail.com>
2025-11-30 17:47:35 +0900

    create vite@latest

--- 004-agent_with_context/todo_frontend/src/App.tsx
+++ 004-agent_with_context/todo_frontend/src/App.tsx
@@ -1,28 +1,35 @@
-import { useEffect, useState } from "react";
-
-import * as api from "./api/task";
-import { Task } from "./entity/task";
-import { NewTaskForm } from "./views/newTask";
-import { ListTaskView } from "./views/taskList";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-    const [taskList, setTasks] = useState<Task[]>([]);
+  const [count, setCount] = useState(0)
 
-    async function reloadTasks() {
-        const tasks = await api.loadTasks();
-        setTasks(tasks);
-    }
-
-    useEffect(() => {
-        reloadTasks();
-    }, []);
-
-    return (
-        <div className="container">
-            <NewTaskForm reloadTasks={reloadTasks} />
-            <ListTaskView taskList={taskList} reloadTasks={reloadTasks} />
-        </div>
-    );
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
